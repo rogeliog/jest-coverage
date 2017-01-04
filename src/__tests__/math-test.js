93c6063aac6cd09f67965224fce58cc07c473645
Noel Martin Llevares <noelmartin@gmail.com>
2017-01-05 10:37:23 +1100

    Modify structure to demo issue.

--- src/__tests__/math-test.js
+++
@@ -1,7 +0,0 @@
-const sum = require('../sum');
-// const div = require('../div');
-// const mult = require('../mult');
-
-it('Works', () => {
-  expect(sum(1, 2)).toBe(3);
-})
---
+++ test/unit/math.test.js
@@ -0,0 +1,7 @@
+const sum = require('../../src/sum/sum');
+const div = require('../../src/div/div');
+const mult = require('../../src/mult/mult');
+
+it('Works', () => {
+  expect(sum(1, 2)).toBe(3);
+})
